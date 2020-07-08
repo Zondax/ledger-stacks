cfdb4dbc91bbaad676804db471b0f47047eb4459
Juan Leni <juan.leni@zondax.ch>
2020-07-08 13:12:21 +0200

    upgrade zemu + update tests

--- tests_zemu/tests/test.js
+++ tests_zemu/tests/test.js
@@ -101,14 +101,10 @@ describe('Basic checks', function () {
 
             // Derivation path. First 3 items are automatically hardened!
             const path = "m/44'/5757'/5'/0/3";
-            const respRequest = app.showAddressAndPubKey(path);
 
-            // Get start screen and loop until it changes
-            await Zemu.sleep(1000);
-            let startScreen = sim.snapshot()
-            while(startScreen === sim.snapshot() ) {
-                await Zemu.sleep(1000);
-            }
+            const respRequest = app.showAddressAndPubKey(path);
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
             // Now navigate the address / path
             await sim.snapshot(`${snapshotPrefixTmp}${snapshotCount++}.png`);
@@ -144,11 +140,8 @@ describe('Basic checks', function () {
             // Do not await.. we need to click asynchronously
             const signatureRequest = app.sign("m/44'/5757'/5'/0/0", blob);
 
-            // Get start screen and loop until it changes
-            let startScreen = sim.snapshot()
-            while(startScreen === sim.snapshot() ) {
-                await Zemu.sleep(1000);
-            }
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
             await sim.clickRight();
             await sim.clickRight();
@@ -178,18 +171,12 @@ describe('Basic checks', function () {
             const app = new BlockstackApp(sim.getTransport());
 
             const blob = Buffer.from("80800000000400143e543243dfcd8c02a12ad7ea371bd07bc91df900000000000000000000000000000000000057293fd439db4be0b4af96a829543690455fdb4b60d6933629b44323dc9c5eb868170e283e1635f4ccc6e2e5067f936eb2b10a50b06eb70c1848f3d3269ddf8c030100000001000102000000000000000000051a3b471808467d33eec688b7a7a75f06aad921ba6e000000000000007b00000000000000000000000000000000000000000000000000000000000000000000", "hex");
-	    // Do not await.. we need to click asynchronously
+            // Do not await.. we need to click asynchronously
             const signatureRequest = app.sign("m/44'/5757'/5'/0/0", blob);
 
-		// Get start screen and loop until it changes
-            let startScreen = sim.snapshot()
-            await Zemu.sleep(1000);
-            while(startScreen === sim.snapshot() ) {
-                await Zemu.sleep(1000);
-            }
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
+
             await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
@@ -222,19 +209,9 @@ describe('Basic checks', function () {
             const blob = Buffer.from("80800000000500143e543243dfcd8c02a12ad7ea371bd07bc91df9000000000000000000000000000000000001da16615641474cb924c7c21ea64ba9398108ee4eeff379b24da7f93ad207a0693b53501ad3a83ee3878475afb86d1a4b532862372d068771a330c3a49489e40d003b471808467d33eec688b7a7a75f06aad921ba6e000000000000000000000000000000000000c386b06eda046b9b19e99506bd694af47286aef3fb358429309ffccea7052837261410014df4c29555e84240461c82b5a0c7dbf18d53551638790659a252efb8030200000000010b68656c6c6f2d776f726c64000000c60a202020202020202028646566696e652d646174612d7661722062617220696e742030290a202020202020202028646566696e652d7075626c696320286765742d6261722920286f6b20287661722d676574206261722929290a202020202020202028646566696e652d7075626c696320287365742d62617220287820696e742920287920696e7429290a202020200302202020202028626567696e20287661722d7365742062617220282f2078200115292920286f6b20287661722d6765742062617229292929", "hex");
             const signatureRequest = app.sign("m/44'/5757'/5'/0/0", blob);
 
-            // Get start screen and loop until it changes
-            await Zemu.sleep(1000);
-            let startScreen = sim.snapshot()
-            while(startScreen === sim.snapshot() ) {
-                await Zemu.sleep(1000);
-            }
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
@@ -261,18 +238,9 @@ describe('Basic checks', function () {
             const blob = Buffer.from("80800000000400143e543243dfcd8c02a12ad7ea371bd07bc91df90000000000000000000000000000000000013a078ceb0c539e278bfa3ab99b0e5d99579ac9c79d0899d91b798390de32521414dded3560f11c212152fc3e2d66af111002139c1597458665264d98c0d3a804030200000000010b68656c6c6f2d776f726c64000000c60a202020202020202028646566696e652d646174612d7661722062617220696e742030290a202020202020202028646566696e652d7075626c696320286765742d6261722920286f6b20287661722d676574206261722929290a202020202020202028646566696e652d7075626c696320287365742d62617220287820696e742920287920696e7429290a2020202020202020202028626567696e20287661722d7365742062617220282f20782079292920286f6b20287661722d6765742062617229292929", "hex");
             const signatureRequest = app.sign("m/44'/5757'/5'/0/0", blob);
 
-            // Get start screen and loop until it changes
-            await Zemu.sleep(1000);
-            let startScreen = sim.snapshot()
-            while(startScreen === sim.snapshot() ) {
-                await Zemu.sleep(1000);
-            }
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
@@ -300,14 +268,9 @@ describe('Basic checks', function () {
             const blob = Buffer.from("808000000004003a8dda5c8785cbba6daec2013bdac06b98202bc30000000000000000000000000000000000004c4c6b830501e8853f4c5b94fa699a6a14b2c07fa1d2b969fe61bcc830aefe3b44cb3a294f33fd615661b6a6b593108c588d92b930ef68225d1bb4417c023ed9030200000000021a143e543243dfcd8c02a12ad7ea371bd07bc91df90b68656c6c6f2d776f726c64077365742d6261720000000200000000000000000000000000000000060000000000000000000000000000000002", "hex");
             const signatureRequest = app.sign("m/44'/5757'/5'/0/0", blob);
 
-            // Get start screen and loop until it changes
-            await Zemu.sleep(1000);
-            let startScreen = sim.snapshot()
-            while(startScreen === sim.snapshot() ) {
-                await Zemu.sleep(1000);
-            }
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
@@ -335,13 +298,10 @@ describe('Basic checks', function () {
             const app = new BlockstackApp(sim.getTransport());
 
             const blob = Buffer.from("808000000005002d89de56fd4db19741957831926e9ba96cf04158000000000000000000000000000000000001c88dc2ad9b081db525b68a04a4e9a021f05d6c8500b43ff01360f255826f3676636bcd0494a55bfd529028fe8c1b1e93ad23b75c31b29cee369d8bf5f643d478003b471808467d33eec688b7a7a75f06aad921ba6e0000000000000000000000000000000000001fc1ecc42a7b62598a6969cc0af77d81992839e203946867e603d4d8d2a3653a7efc00d16423b035f82d5550f26d3d59205b0cf578a93618c3eb7f50dc12f73c030200000000021a143e543243dfcd8c02a12ad7ea371bd07bc91df90b68656c6c6f2d776f726c64077365742d6261720000000200000000000000000000000000000000060000000000000000000000000000000002", "hex");
+            const signatureRequest = app.sign("m/44'/5757'/5'/0/0", blob);
 
-            // Get start screen and loop until it changes
-            await Zemu.sleep(1000);
-            let startScreen = sim.snapshot()
-            while(startScreen === sim.snapshot() ) {
-                await Zemu.sleep(1000);
-            }
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
             await sim.clickRight();
             await sim.clickRight();
