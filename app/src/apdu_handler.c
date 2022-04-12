4b9a78a6b72f991d781f42f6a5c32d6a06e1627d
Natanael <neithanmo@gmail.com>
2022-04-12 10:13:14 -0600

    adds:     - New instruction to retrieve an identity key using m/888'/0' path     - add handler for such instruction

--- app/src/apdu_handler.c
+++ app/src/apdu_handler.c
@@ -53,6 +53,14 @@ __Z_INLINE void handleGetAddrSecp256K1(volatile uint32_t *flags, volatile uint32
     THROW(APDU_CODE_OK);
 }
 
+__Z_INLINE void handleGetAuthPubKey(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
+    extractHDPath(rx, OFFSET_DATA);
+
+    *tx = app_fill_auth_pubkey(addr_secp256k1);
+    THROW(APDU_CODE_OK);
+}
+
+
 __Z_INLINE void handleSignSecp256K1(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
     if (!process_chunk(rx)) {
         THROW(APDU_CODE_OK);
@@ -103,6 +111,11 @@ void handleApdu(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
 
                 case INS_SIGN_SECP256K1: {
                     handleSignSecp256K1(flags, tx, rx);
+                    break;
+                }
+
+                case INS_GET_AUTH_PUBKEY: {
+                    handleGetAuthPubKey(flags, tx, rx);
                     break;
                 }
 
--- app/src/common/app_main.h
+++ app/src/common/app_main.h
@@ -34,6 +34,7 @@
 #define INS_GET_VERSION                 0x00
 #define INS_GET_ADDR_SECP256K1          0x01
 #define INS_SIGN_SECP256K1              0x02
+#define INS_GET_AUTH_PUBKEY             0x03
 
 void app_init();
 
