eb743f253bc09480d46337d07b4ca950bf6529ae
Natanael <neithanmo@gmail.com>
2022-11-08 14:46:21 +0100

    move hash function to bolos as it is where SDK declarations are, and update jwt and c32 modules accordingly

--- app/rust/src/bolos.rs
+++ app/rust/src/bolos.rs
@@ -1,4 +1,5 @@
 //! Rust interfaces to Ledger SDK APIs.
+pub const SHA256_LEN: usize = 32;
 
 extern "C" {
     fn _zemu_log_stack(buffer: *const u8);
@@ -10,3 +11,36 @@ pub fn c_zemu_log_stack<S: AsRef<[u8]>>(s: S) {
 }
 #[cfg(test)]
 pub fn c_zemu_log_stack<S: AsRef<[u8]>>(_s: S) {}
+
+// extern function that uses the device sdk to compute a hash
+extern "C" {
+    pub fn hash_sha256(in_data: *const u8, in_len: u16, out: *mut u8);
+}
+
+#[derive(Debug)]
+pub struct OutputTooSmall;
+
+#[cfg(not(any(test, fuzzing)))]
+pub fn sha256(data: &[u8], output: &mut [u8]) -> Result<(), OutputTooSmall> {
+    if output.len() < SHA256_LEN {
+        return Err(OutputTooSmall);
+    }
+
+    unsafe {
+        hash_sha256(data.as_ptr(), data.len() as _, output.as_mut_ptr());
+    }
+
+    Ok(())
+}
+
+#[cfg(any(test, fuzzing))]
+pub fn sha256(data: &[u8], out: &mut [u8]) -> Result<(), OutputTooSmall> {
+    use sha2::Digest;
+    use sha2::Sha256;
+    let digest = Sha256::digest(data);
+    if out.len() != digest.len() {
+        return Err(OutputTooSmall);
+    }
+    out.copy_from_slice(digest.as_slice());
+    Ok(())
+}
